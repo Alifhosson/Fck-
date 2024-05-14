f5f4543842eb364ea729f9efd8dc473d5f683c10
FUCK-YOU <153631607+Alifhosson@users.noreply.github.com>
2024-05-14 21:30:30 +0600

    Create album2.js

---
+++ scripts/commands/album2.js
@@ -0,0 +1,88 @@
+module.exports.config = {
+    name: "album2",
+    version: "1.0.3",
+    permission: 1,
+    credits: "Alif",
+    description: "album video ",
+    prefix: true, 
+    category: "Video list", 
+    usages: "album2",
+    cooldowns: 5,
+    dependencies: {
+        "axios": ""
+    }
+};
+
+}, module.exports.run = async function({
+	event: e,
+	api: a,
+	args: n
+}) {
+	if (!n[0]) return a.sendMessage("    ♚═══ ∰𝙰𝙻𝙱𝚄𝙼 𝚂𝚃𝙰𝚁𝚃 ☚ ═══♚\n\n   ∰⇨𝙰𝙻𝙱𝚄𝙼 𝙲𝚁𝙴𝙳𝙸𝚃 𝙱𝚈 𝚁𝙾𝙼𝙸𝙼ᬊᬁ \n                        👑 \n\n                   ♬ 𝙰𝙿𝙸 𝙱𝚈 𝚁𝙾𝙼𝙸𝙼 ♪\n                      ★         👑        ★\n\n                      𝙵𝙾𝙾𝚃𝙱𝙰𝙻𝙻 !\n\n                   ━━━━━━━━━━━━━━━━━━━━━\n\n ✇1 -  𝙵𝙾𝙾𝚃𝙱𝙰𝙻𝙻   𝚅𝙸𝙳𝙴𝙾 !\n\n  ✇2 -  𝙼𝙴𝚂𝚂𝙸 1 !\n\n ✇3 -  𝙽𝙴𝚈𝙼𝙰𝚁  !\n\n  ✇4 -  𝚁𝙾𝙽𝙰𝙻𝙳𝙾 !\n\n   ━━━━━━━━━━━━━━━━━━━━━\n\n               ★        👑         ★\n                    𝙵𝚁𝙴𝙴 𝙵𝙸𝚁𝙴 !\n\n    ━━━━━━━━━━━━━━━━━━━━━\n\n ✇5 -  𝙵𝚁𝙴𝙴 𝙵𝙸𝚁𝙴 𝙴𝙳𝙸𝚃𝚉 𝚅𝙸𝙳𝙴𝙾   !\n\n  ✇6 -  𝙵𝚁𝙴𝙴 𝙵𝙸𝚁𝙴 𝙴𝙳𝙸𝚃𝚉 𝚂𝙰𝙳 𝚅𝙸𝙳𝙴𝙾 !\n\n ✇7 -  𝚆𝙷𝙸𝚃𝙴 01 !\n\n  ✇8 -  𝙱𝙾𝙽𝙴𝚇4 𝙵𝙵 !\n\n ✇9 -  𝙼1𝙽𝚇 !\n\n       ━━━━━━━━━━━━━━━━━━━━━\n\n               ★          👑        ★\n\n                    𝙲𝚁𝙸𝙲𝙺𝙴𝚃 !\n\n   ━━━━━━━━━━━━━━━━━━━━━\n\n✇10 - 𝚂𝙰𝙺𝙸𝙱 𝙰𝙻 𝙷𝙰𝚂𝙰𝙽 !\n\n   ━━━━━━━━━━━━━━━━━━━━━\n\n               ★          👑        ★\n                         𝚂𝙸𝙶𝙼𝙰 !\n\n  ━━━━━━━━━━━━━━━━━━━━━\n ✇11 - 𝙷𝙰𝚉𝚈 𝙼𝙰𝙽 !\n\n   ━━━━━━━━━━━━━━━━━━━━━\n\n               ★          👑        ★\n                       𝙸𝚂𝙻𝙰𝙼𝙸𝙲 !\n\n ━━━━━━━━━━━━━━━━━━━━━\n\n ✇12 - 𝙸𝚂𝙻𝙰𝙼𝙸𝙲 𝚅𝙸𝙳𝙴𝙾 !\n\n ✇13 - 𝙰𝙳𝙽𝙰𝙽 𝙷𝚄𝙹𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 !\n    ━━━━━━━━━━━━━━━━━━━━━\n\n               ★          👑        ★\n                         𝙻𝙾𝚅𝙴  !\n\n    ━━━━━━━━━━━━━━━━━━━━━\n ✇14  -  𝙻𝙾𝚅𝙴 𝚅𝙸𝙳𝙴𝙾 !\n\n    ★━━━━━━━━━━━━━━━━━━━★\n  ✇15  -  18+- 𝙷𝙾𝚁𝙽𝚈 𝚅𝙸𝙳𝙴𝙾 !\n\n   ☆━━━━━━━━━━━━━━━━━━━☆\n\n                 ⇩  𝙰𝙻𝙱𝚄𝙼 𝙴𝙽𝙳  ⚚\n\n   ⚚━━━━━━━━━━━━━━━━━━━⚚\n\n          ★❦𝙴𝙽𝙹𝙾𝚈 𝚁4𝙼1𝙼 𝙰𝙻𝙱𝚄𝙼༒\n\n             ", e.threadID, ((a, n) => {
+		global.client.handleReply.push({
+			name: this.config.name,
+			messageID: n.messageID,
+			author: e.senderID,
+			type: "create"
+		})
+	}), e.messageID)
+	}, module.exports.handleReply = async ({
+	api: e,
+	event: a,
+	client: n,
+	handleReply: t,
+	Currencies: s,
+	Users: i,
+	Threads: o
+	}) => {
+	var { p, h } = linkanh();
+
+	if ("create" === t.type) {
+		const n = (await p.get(h)).data.data;
+		const Romim = (await p.get(h)).data.shaon;
+		const ls = (await p.get(h)).data.count;
+		let t = (await p.get(n, {
+			responseType: "stream"
+		})).data;
+		return e.sendMessage({
+			body: `𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝙰𝙿𝙸 𝚂𝙴𝙽𝙳 𝙼𝙴𝚂𝚂𝙰𝙶𝙴.!`,
+			attachment: t
+		}, a.threadID, a.messageID)
+	}
+
+	function linkanh() {
+				const p = require("axios");
+				if ("1" == a.body)
+						var h = "https://video-api-akug.onrender.com/football";
+				else if ("2" == a.body)
+				 var   h = "https://video-api-akug.onrender.com/messi";
+				else if ("3" == a.body)
+				 var   h = "https://video-api-akug.onrender.com/Neymar";
+				else if ("4" == a.body)
+					var  h = "https://video-api-akug.onrender.com/ronaldo";
+				else if ("5" == a.body)
+					var  h = "https://video-api-akug.onrender.com/editff";
+				else if ("6" == a.body)
+					var  h = "https://video-api-akug.onrender.com/ffsad";
+				else if ("7" == a.body)
+					var  h = "https://video-api-akug.onrender.com/white01";
+				else if ("8" == a.body)
+					var  h = "https://video-api-akug.onrender.com/bonex";
+				else if ("9" == a.body)
+				 var   h = "https://video-api-akug.onrender.com/m1nx";
+				else if ("10" == a.body)
+				 var  h = "https://video-api-akug.onrender.com/sakib";
+					else if ("11" == a.body)
+					 var  h = "https://video-api-akug.onrender.com/hazy";
+					else if ("12" == a.body)
+					var  h = "https://video-api-akug.onrender.com/islamic";
+				 else if ("13" == a.body)
+				 var  h = "https://video-api-akug.onrender.com/adnan";
+				 else if ("14" == a.body)
+				 var  h = "https://video-api-akug.onrender.com/status";
+		else if ("15" == a.body)
+				 var  h =
+"https://video-api-akug.onrender.com/sexy";
+				return { p, h };
+		}
+};
